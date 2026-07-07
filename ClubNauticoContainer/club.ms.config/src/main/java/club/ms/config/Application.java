89bc934f8aee55572ce4b752ee6439860ca2bcc6
cniki2651-arch <cniki2651@gmail.com>
2026-07-07 18:42:13 -0500

    Agregado Config Server (ClubNauticoMSConfig)

---
+++ ClubNauticoContainer/club.ms.config/src/main/java/club/ms/config/Application.java
@@ -0,0 +1,14 @@
+package club.ms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
