b22ae2738d3c935a1d294956b78d2ccb89d9633c
cniki2651-arch <cniki2651@gmail.com>
2026-07-07 16:06:38 -0500

    Estructura inicial de ms-socios

---
+++ ClubNauticoMSSocios/src/main/java/club/ms/socios/ClubNauticoMsSociosApplication.java
@@ -0,0 +1,13 @@
+package club.ms.socios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubNauticoMsSociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubNauticoMsSociosApplication.class, args);
+	}
+
+}
---
+++ ClubNauticoMSSocios/src/test/java/club/ms/socios/ClubNauticoMsSociosApplicationTests.java
@@ -0,0 +1,13 @@
+package club.ms.socios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoMsSociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
